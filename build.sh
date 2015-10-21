cd24ec5d040085f19acc44e04ff1da1aae3f1e70
luckyadam <weitaozsh@gmail.com>
2015-10-21 12:59:18 +0800

    增加CNAME

--- build.sh
+++ build.sh
@@ -2,13 +2,15 @@
 rm -rf public || exit 0;
 mkdir public;
 hexo optimize;
-if [ "$1" = "false" ] 
+if [ "$1" = "false" ]
 # not a pull request, deploy to github pages
 then ( cd public
   git init
   git config user.name "luckyadam"
   git config user.email "weitaozsh@gmail.com"
-  git add .
+  echo "diao.li" > CNAME
+  touch .
+  git add -A .
   git commit -m "Deployed to Github Pages [skip ci]"
   git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages
 )
